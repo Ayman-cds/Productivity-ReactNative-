d7486b69be4bbf1d03a93ccab644c9b3817815e3
Ayman-cds <business.aymanadil@gmail.com>
2021-06-12 11:57:20 +0300

    draggable flat list error

--- components/Focus.js
+++ components/Focus.js
@@ -1,8 +1,12 @@
-import React, { useState, useCallback } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import DraggableFlatList, {
-    RenderItemParams,
-} from 'react-native-draggable-flatlist';
+// import React, { useState, useCallback } from 'react';
+// import { View, Text, TouchableOpacity } from 'react-native';
+// import DraggableFlatList, {
+//     RenderItemParams,
+// } from 'react-native-draggable-flatlist';
+
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import DraggableFlatList from 'react-native-draggable-flatlist';
 
 const exampleData = [
     {
@@ -22,48 +26,46 @@ const exampleData = [
         name: 'fourth thing',
     },
 ];
+// const exampleData = [...Array(20)].map((d, index) => ({
+//   key: `item-${index}`, // For example only -- don't use index as your key!
+//   label: index,
+//   backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index *
+//   5}, ${132})`
+// }));
 
 export default function Focus() {
     const [data, setData] = useState(exampleData);
-
-    const renderItem = useCallback(
-        ({ item, index, drag, isActive }: RenderItemParams<Item>) => {
-            return (
-                <TouchableOpacity
+    const renderItem = ({ item, index, drag, isActive }) => {
+        return (
+            <TouchableOpacity
+                style={{
+                    height: 100,
+                    backgroundColor: isActive ? 'blue' : item.backgroundColor,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                onLongPress={drag}
+            >
+                <Text
                     style={{
-                        height: 100,
-                        backgroundColor: isActive
-                            ? 'red'
-                            : item.backgroundColor,
-                        alignItems: 'center',
-                        justifyContent: 'center',
+                        fontWeight: 'bold',
+                        color: 'white',
+                        fontSize: 32,
                     }}
-                    onLongPress={drag}
                 >
-                    <Text
-                        style={{
-                            fontWeight: 'bold',
-                            color: 'white',
-                            fontSize: 32,
-                        }}
-                    >
-                        Item
-                    </Text>
-                </TouchableOpacity>
-            );
-        },
-        []
-    );
+                    {item.name}
+                </Text>
+            </TouchableOpacity>
+        );
+    };
 
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <DraggableFlatList
                 data={data}
                 renderItem={renderItem}
-                keyExtractor={(item, index) => {
-                    item.key;
-                }}
-                onDragEnd={({ data }) => setData(data)}
+                keyExtractor={(item, index) => `draggable-item-${item.key}`}
+                onDragEnd={({ d }) => setData({ d })}
             />
         </View>
     );
