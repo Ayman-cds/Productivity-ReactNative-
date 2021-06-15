2a52c780a194fdb0b75d4067ad13f54ae5cabd87
Ayman-cds <business.aymanadil@gmail.com>
2021-06-15 21:19:58 +0300

    Adds gradient background

--- components/Home.js
+++ components/Home.js
@@ -9,6 +9,7 @@ import {
     StackedBarChart,
 } from 'react-native-chart-kit';
 import { Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Home() {
     const chartConfig = {
@@ -23,64 +24,92 @@ export default function Home() {
     };
 
     return (
-        <View style={styles.chart}>
-            <LineChart
-                data={{
-                    labels: [
-                        'January',
-                        'February',
-                        'March',
-                        'April',
-                        'May',
-                        'June',
-                    ],
-                    datasets: [
-                        {
-                            data: [
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
+        <View style={styles.container}>
+            <LinearGradient
+                // Background Linear Gradient
+                colors={['#071E3D', '#21E6C1']}
+                style={styles.background}
+            >
+                <Text style={styles.greeting}> Hi Ayman,</Text>
+                <View style={styles.chart}>
+                    <LineChart
+                        data={{
+                            labels: [
+                                'Mon',
+                                'Tue',
+                                'Wed',
+                                'Thur',
+                                'Fri',
+                                'Sat',
+                                'Sun',
                             ],
-                        },
-                    ],
-                }}
-                width={Dimensions.get('window').width} // from react-native
-                height={220}
-                yAxisLabel="$"
-                yAxisSuffix="k"
-                yAxisInterval={1} // optional, defaults to 1
-                chartConfig={{
-                    backgroundColor: '#278EA5',
-                    backgroundGradientFrom: '#21E6C1',
-                    backgroundGradientTo: '#071E3D',
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    labelColor: (opacity = 1) =>
-                        `rgba(255, 255, 255, ${opacity})`,
-                    style: {
-                        borderRadius: 16,
-                    },
-                    propsForDots: {
-                        r: '6',
-                        strokeWidth: '2',
-                        stroke: '#071E3D',
-                    },
-                }}
-                bezier
-                style={{
-                    marginVertical: 8,
-                    borderRadius: 16,
-                }}
-            />
+                            datasets: [
+                                {
+                                    data: [
+                                        Math.round(Math.random() * 10),
+                                        Math.round(Math.random() * 10),
+                                        Math.round(Math.random() * 10),
+                                        Math.round(Math.random() * 10),
+                                        Math.round(Math.random() * 10),
+                                    ],
+                                },
+                            ],
+                        }}
+                        width={Dimensions.get('window').width} // from react-native
+                        height={220}
+                        yAxisLabel=""
+                        yAxisSuffix="h"
+                        yAxisInterval={1} // optional, defaults to 1
+                        chartConfig={{
+                            backgroundColor: '#278EA5',
+                            backgroundGradientFrom: '#21E6C1',
+                            backgroundGradientTo: '#071E3D',
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) =>
+                                `rgba(255, 255, 255, ${opacity})`,
+                            labelColor: (opacity = 1) =>
+                                `rgba(255, 255, 255, ${opacity})`,
+                            style: {
+                                borderRadius: 16,
+                            },
+                            propsForDots: {
+                                r: '6',
+                                strokeWidth: '2',
+                                stroke: '#071E3D',
+                            },
+                        }}
+                        bezier
+                        style={{
+                            marginVertical: 8,
+                            borderRadius: 16,
+                        }}
+                    />
+                </View>
+            </LinearGradient>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: '#071e3d',
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: 3000,
+    },
     chart: {
-        paddingTop: 100,
+        paddingTop: 30,
+    },
+    greeting: {
+        paddingTop: 30,
+        padding: 10,
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#21E6C1',
     },
 });
