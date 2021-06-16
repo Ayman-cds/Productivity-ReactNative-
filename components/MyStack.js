f7853afbd6fd6bb6b426b8601d707474f9c1032e
Ayman-cds <business.aymanadil@gmail.com>
2021-06-16 11:33:36 +0300

    adds login screen

--- components/MyStack.js
+++ components/MyStack.js
@@ -2,10 +2,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Focus from './Focus';
 import Home from './Home';
 import React from 'react';
+import Login from './Login';
 const Stack = createStackNavigator();
 const MyStack = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Focus" component={Focus} />
         </Stack.Navigator>
