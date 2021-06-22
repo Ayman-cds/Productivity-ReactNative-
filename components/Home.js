a06cc1e9bc44edc89295b5f9795fb54546bd76fe
Ayman-cds <business.aymanadil@gmail.com>
2021-06-22 16:37:59 +0300

    Adds Daily Stats

--- components/Home.js
+++ components/Home.js
@@ -3,11 +3,12 @@ import {
     View,
     Text,
     StyleSheet,
-    Button,
     ScrollView,
     TouchableOpacity,
     ActivityIndicator,
 } from 'react-native';
+import Button from './Button';
+
 import {
     LineChart,
     BarChart,
@@ -24,6 +25,7 @@ import * as firebase from 'firebase';
 import { connect } from 'react-redux';
 import { fetchUser } from '../redux/actions';
 import 'firebase/firestore';
+import Carousel from 'react-native-snap-carousel';
 
 function Home(props) {
     const [taskItems, setTaskItems] = useState([]);
@@ -75,61 +77,72 @@ function Home(props) {
                         color="white"
                     />
                 </View>
-                <View style={styles.chart}>
-                    <LineChart
-                        data={{
-                            labels: [
-                                'Mon',
-                                'Tue',
-                                'Wed',
-                                'Thur',
-                                'Fri',
-                                'Sat',
-                                'Sun',
-                            ],
-                            datasets: [
-                                {
-                                    data: [
-                                        Math.round(Math.random() * 10),
-                                        Math.round(Math.random() * 10),
-                                        Math.round(Math.random() * 10),
-                                        Math.round(Math.random() * 10),
-                                        Math.round(Math.random() * 10),
-                                    ],
+                <ScrollView horizontal={true}>
+                    <View style={styles.dailyStatsItem}>
+                        <Text style={styles.dailyStatsText}>1h 20m</Text>
+                        <Text style={styles.dailyStatsPercentage}>
+                            20% Greater Than Average
+                        </Text>
+                        <Text style={styles.dailyStatsPercentage}>
+                            10% > yesterday
+                        </Text>
+                    </View>
+                    <View style={styles.chart}>
+                        <LineChart
+                            data={{
+                                labels: [
+                                    'Mon',
+                                    'Tue',
+                                    'Wed',
+                                    'Thur',
+                                    'Fri',
+                                    'Sat',
+                                    'Sun',
+                                ],
+                                datasets: [
+                                    {
+                                        data: [
+                                            Math.round(Math.random() * 10),
+                                            Math.round(Math.random() * 10),
+                                            Math.round(Math.random() * 10),
+                                            Math.round(Math.random() * 10),
+                                            Math.round(Math.random() * 10),
+                                        ],
+                                    },
+                                ],
+                            }}
+                            width={Dimensions.get('window').width - 10} // from react-native
+                            height={200}
+                            yAxisSuffix="h"
+                            yAxisInterval={1} // optional, defaults to 1
+                            chartConfig={{
+                                backgroundColor: '#278EA5',
+                                backgroundGradientFrom: '#071E3D',
+                                backgroundGradientTo: '#278EA5',
+                                opacity: 1,
+                                decimalPlaces: 2, // optional, defaults to 2dp
+                                // yAxisLabel: 'Hours of fsdfsdfsdfsocus',
+                                color: (opacity = 1) =>
+                                    `rgba(255, 255, 255, ${opacity})`,
+                                labelColor: (opacity = 1) =>
+                                    `rgba(255, 255, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
                                 },
-                            ],
-                        }}
-                        width={Dimensions.get('window').width} // from react-native
-                        height={220}
-                        yAxisLabel=""
-                        yAxisSuffix="h"
-                        yAxisInterval={1} // optional, defaults to 1
-                        chartConfig={{
-                            backgroundColor: '#278EA5',
-                            backgroundGradientFrom: '#071E3D',
-                            backgroundGradientTo: '#278EA5',
-                            opacity: 1,
-                            decimalPlaces: 2, // optional, defaults to 2dp
-                            color: (opacity = 1) =>
-                                `rgba(255, 255, 255, ${opacity})`,
-                            labelColor: (opacity = 1) =>
-                                `rgba(255, 255, 255, ${opacity})`,
-                            style: {
+                                propsForDots: {
+                                    r: '6',
+                                    strokeWidth: '2',
+                                    stroke: '#071E3D',
+                                },
+                            }}
+                            bezier
+                            style={{
+                                marginVertical: 8,
                                 borderRadius: 16,
-                            },
-                            propsForDots: {
-                                r: '6',
-                                strokeWidth: '2',
-                                stroke: '#071E3D',
-                            },
-                        }}
-                        bezier
-                        style={{
-                            marginVertical: 8,
-                            borderRadius: 16,
-                        }}
-                    />
-                </View>
+                            }}
+                        />
+                    </View>
+                </ScrollView>
                 <LinearGradient
                     Background
                     Linear
@@ -155,12 +168,22 @@ function Home(props) {
                             <ActivityIndicator size="large" color="#071E3D" />
                         )}
                     </ScrollView>
-                    <TouchableOpacity
+                    <Button
+                        text="Focus Mode"
+                        onPress={() => props.navigation.push('Focus')}
+                        style={{
+                            alignSelf: 'center',
+                            backgroundColor: '#278EA5',
+                            opacity: 0.4,
+                            // marginVertical: hp(2,
+                        }}
+                    />
+                    {/* <TouchableOpacity
                         onPress={() => props.navigation.push('Focus')}
                         style={styles.button}
                     >
                         <Text style={styles.buttonText}>FOCUS MODE</Text>
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
                 </LinearGradient>
             </LinearGradient>
         </View>
@@ -172,6 +195,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+    },
+    dailyStatsText: {
+        fontSize: 60,
+        color: '#fff',
+        // opacity: 0.5,
     },
     logout: {
         paddingTop: 25,
@@ -185,6 +213,22 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         borderRadius: 10,
         flexDirection: 'row',
+    },
+    dailyStatsItem: {
+        width: Dimensions.get('window').width - 20,
+        height: 200,
+        backgroundColor: '#39A6A3',
+        opacity: 0.4,
+        marginTop: 40,
+        padding: 10,
+        marginHorizontal: 10,
+        borderRadius: 10,
+        // flexDirection: 'row',
+    },
+    dailyStatsPercentage: {
+        fontSize: 20,
+        color: '#fff',
+        opacity: 0.5,
     },
     uncompletedTasksScroll: {
         height: 300,
@@ -231,7 +275,8 @@ const styles = StyleSheet.create({
         paddingBottom: 15,
         marginLeft: 30,
         marginRight: 30,
-        backgroundColor: '#21E6C1',
+        backgroundColor: '#278EA5',
+        opacity: 0.2,
         borderRadius: 10,
     },
     buttonText: {
