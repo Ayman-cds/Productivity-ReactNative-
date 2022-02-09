40e5ecf1ae1ffd1908544e63dfbf3b151bd86659
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-09 13:20:07 +0300

    adding configs

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import MyStack from './components/MyStack';
-import firebaseConfig from './components/FirebaseConfig';
-import firebase from 'firebase/app';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './redux/reducers';
-import thunk from 'redux-thunk';
-import { YellowBox } from 'react-native';
-import Home from './components/Home';
-import Pomodoro from './Pomodoro';
-YellowBox.ignoreWarnings(['Require cycle']);
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MyStack from "./components/MyStack";
+import firebaseConfig from "./components/FirebaseConfig";
+import firebase from "firebase/app";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+import { YellowBox } from "react-native";
+import Home from "./components/Home";
+import Pomodoro from "./Pomodoro";
+YellowBox.ignoreWarnings(["Require cycle"]);
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 export default function App() {
-    return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <MyStack />
-            </NavigationContainer>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </Provider>
+  );
 }
