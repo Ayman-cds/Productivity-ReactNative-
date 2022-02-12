7e37af40e5a1508992d57a696721e4abe5ebebc8
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-12 15:05:19 +0300

    REFACTORING EVERYTHING (using typescript)

---
+++ config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyATuXp8FsWMNaUh8KTZDL3xaRePLhQWbrg",
+  authDomain: "productivity-app-3f067.firebaseapp.com",
+  projectId: "productivity-app-3f067",
+  storageBucket: "productivity-app-3f067.appspot.com",
+  messagingSenderId: "456512857721",
+  appId: "1:456512857721:web:94d391ee15d08638b616a8",
+  measurementId: "G-D1BZ9T2G1N",
+};
