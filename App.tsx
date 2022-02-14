ebedeebcb33e3484fd48162ef4ddc9ca5ad4e9bf
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-14 23:51:15 +0300

    adding navigation stack

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
+import MyStack from "./navigation/MyStack";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const colorScheme = 
+  ();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <View>
-        <Text>Hello</Text>
-      </View>
+     <Provider store={store}>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </Provider>
     );
   }
 }
---
+++ navigation/MyStack.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/Login";
+import Signup from "../screens/Signup";
+const Stack = createStackNavigator();
+const MyStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
+  );
+};
+export default MyStack;
