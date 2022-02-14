7f81cfe570d32602808bfd416abd6abff4d74a59
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-14 23:42:46 +0300

    App:

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,10 +14,9 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <View>
+        <Text>Hello</Text>
+      </View>
     );
   }
 }
