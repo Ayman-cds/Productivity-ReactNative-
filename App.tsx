a3672472c21c6092f4c5538a81e1a02425e4fa47
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-14 23:40:21 +0300

    added jsx to ts config

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
