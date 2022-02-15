cc0a56158883359348340fd6ecac6eb1ec6a187a
Ayman <75167679+Ayman-cds@users.noreply.github.com>
2022-02-16 00:18:47 +0300

    App:

--- App.tsx
+++ App.tsx
@@ -1,28 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
 import MyStack from "./navigation/MyStack";
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = 
-  ();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-     <Provider store={store}>
+  return (
+    <Provider store={store}>
       <NavigationContainer>
         <MyStack />
       </NavigationContainer>
     </Provider>
-    );
-  }
+  );
 }
